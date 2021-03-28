1bfe8ad98b6d9e783fc479c1d5d5bfcb20295ca4
Milen Ivanov <58393766+milen92sl@users.noreply.github.com>
2021-03-29 00:40:19 +0300

    Add Files from Exam Prep EF.Core 2021

---
+++ EXAMS/SoftJail-EXAM 14 AUG 2020/SoftJail/DataProcessor/ImportDto/DepartmentCellInputModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SoftJail.DataProcessor.ImportDto
+{
+    public class DepartmentCellInputModel
+    {
+        [Required]
+        [StringLength(25, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        public List<CellInputModel> Cells { get; set; }
+    }
+
+    public class CellInputModel
+    {
+        [Range(1,1000)]
+        public int  CellNumber { get; set; }
+        public bool HasWindow { get; set; }
+    }
+}
+
+//{
+//"Name": "",
+//"Cells": [
+//{
+//    "CellNumber": 101,
+//    "HasWindow": true
+//},
+//{
+//    "CellNumber": 102,
+//    "HasWindow": false
+//}
+//]
---
+++ EXAMS/SoftJail-EXAM 14 AUG 2020/SoftJail/DataProcessor/ImportDto/OfficersPrisonersInputModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+using SoftJail.Data.Models.Enums;
+
+namespace SoftJail.DataProcessor.ImportDto
+{
+    [XmlType("Officer")]
+    public class OfficersPrisonersInputModel
+    {
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [Range(typeof(decimal),"0","79228162514264337593543950335")]
+        [XmlElement("Money")]
+        public decimal Money { get; set; }
+
+        [EnumDataType(typeof(Position))]
+        [XmlElement("Position")]
+        public string Position { get; set; }
+
+        [EnumDataType(typeof(Weapon))]
+        [XmlElement("Weapon")] 
+        public string Weapon { get; set; }
+
+
+        [XmlElement("DepartmentId")]
+        public int DepartmentId { get; set; }
+
+
+        [XmlArray("Prisoners")]
+        public List<PrisonerIdInputModel> Prisoners { get; set; }
+    }
+    [XmlType("Prisoner")]
+    public class PrisonerIdInputModel
+    {
+        [XmlAttribute("Id")]
+        public int Id { get; set; }
+    }
+}
+
+//•	Id – integer, Primary Key
+//•	FullName – text with min length 3 and max length 30 (required)
+//•	Salary – decimal (non-negative, minimum value: 0) (required)
+//•	Position - Position enumeration with possible values: “Overseer, Guard, Watcher, Labour” (required)
+//•	Weapon - Weapon enumeration with possible values: “Knife, FlashPulse, ChainRifle, Pistol, Sniper” (required)
+//•	DepartmentId - integer, foreign key (required)
+//•	Department – the officer's department (required)
+//•	OfficerPrisoners - collection of type OfficerPrisoner
+
+
+
+//<Officer>
+//<Name>Minerva Kitchingman</Name>
+//<Money>2582</Money>
+//<Position>Invalid</Position>
+//<Weapon>ChainRifle</Weapon>
+//<DepartmentId>2</DepartmentId>
+//<Prisoners>
+//<Prisoner id="15" />
+//</Prisoners>
+//</Officer>
