1c5690d3f42c2303473eb74162c995af17914251
Milen Ivanov <58393766+milen92sl@users.noreply.github.com>
2021-04-02 20:28:26 +0300

    Add files from 02.ExamPrep

---
+++ EXAMS/ExamPrep_Niki/VaporStore/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace VaporStore.Data
+{
+	public static class Configuration
+	{
+		public static string ConnectionString =
+			@"Server=DESKTOP-2LBS7U6\SQLEXPRESS;Database=VaporStore;Trusted_Connection=True";
+	}
+}
